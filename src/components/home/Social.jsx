65d139ffcddfaec5cc2cf52e41e91368fdd70a69
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2023-02-23 14:02:20 -0500

    Add home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+      <a href='https://www.instagram.com/potatocode_' className='home__social-icon' target='_blank' rel='noreferrer'>
+        <i class='uil uil-instagram'></i>
+      </a>
+
+      <a href='https://github.com/nicolasdelrosario' className='home__social-icon' target='_blank' rel='noreferrer'>
+        <i class='uil uil-github-alt'></i>
+      </a>
+
+      <a href='https://www.linkedin.com/in/nicolasdelrosario' className='home__social-icon' target='_blank' rel='noreferrer'>
+        <i class='uil uil-linkedin-alt'></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
